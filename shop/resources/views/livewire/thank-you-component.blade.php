95e4535157792d6be255d4cc37c37404d6b6084f
MustaphaBousilDev <bousilmustapha@gmail.com>
2023-04-04 11:24:32 +0000

    finish thanks page

--- shop/resources/views/livewire/thank-you-component.blade.php
+++ shop/resources/views/livewire/thank-you-component.blade.php
@@ -1,4 +1,4 @@
-<div>
-    <h1>Thanks Mother fucker</h1>
-    <a href="{{route('shop')}}">continue shoppinf</a>
+<div style="height:100vh" class="  h-full bg-color-gray-background-light flex flex-col justify-center items-center">
+    <h1 style='font-size:5rem' class='text-4xl'>Thanks For Shopping</h1>
+    <a href="{{route('shop')}} " class="bg-color-red-button flex py-2 px-4 rounded-md  text-while text-lg" >continue shopping</a>
 </div>
